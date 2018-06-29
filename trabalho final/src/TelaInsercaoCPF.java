7578e8c2a8ec48ae0fd3e9f69acd5c12a2119b06
gabrielmouallem <gabriel.unifei2017@gmail.com>
2018-06-29 16:54:28 -0300

    cadastraCliente e consultaCliente implementados

---
+++ trabalho final/src/TelaInsercaoCPF.java
@@ -0,0 +1,33 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gabriel
+ */
+public class TelaInsercaoCPF extends JFrame implements ActionListener{
+    
+    public TelaInsercaoCPF()
+    {
+        
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
