1e10a8b64c3b2ebb59e1b28d7cc949bd6625cde0
Gabriel Mouallem <gabriel.unifei2017@gmail.com>
2018-07-01 16:02:43 -0300

    adicionados consultaFatCliente e ConsultaFatProduto

--- trabalho final/src/ControleCliente.java
+++ trabalho final/src/ControleCliente.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,63 +17,62 @@ import java.io.ObjectOutputStream;
  * @author Gabriel
  */
 public class ControleCliente {
-    ArrayList <Cliente> clientes;
 
-    public ControleCliente(){
+    ArrayList<Cliente> clientes;
+
+    public ControleCliente() {
         this.clientes = new ArrayList<>();
-        try{
+        try {
             this.lerClientes(); // le arquivo e preenche array de produtos
             System.console().writer().println("arquivo clientes.ser lido");
-        }
-        catch(Exception exc){
+        } catch (Exception exc) {
             System.console().writer().println("erro ao ler arquivo de clientes");
         }
     }
-    
-    public void cadastraCliente (String nome, String endereco, String email, String cpf){
-        Cliente cliente = new Cliente (nome, endereco, email, cpf);
-        try{
+
+    public void cadastraCliente(String nome, String endereco, String email, String cpf) {
+        Cliente cliente = new Cliente(nome, endereco, email, cpf);
+        try {
             clientes.add(cliente);
             this.gravarClientes();
             System.console().writer().println("Cliente " + nome + " do cpf " + cpf + " adicionado");
-        }
-        catch(Exception exc){
-            System.console().writer().println("Erro ao gravar clientes");            
+        } catch (Exception exc) {
+            System.console().writer().println("Erro ao gravar clientes");
         }
     }
 
     //Recebe CPF do cliente e caso ele exista mostra informações dele, se nao gera mensagem de erro
-    public void consultaCliente (String cpf){
+    public void consultaCliente(String cpf) {
         String saida = "Cliente não encontrado.\n";
-        for (int i=0;i<clientes.size();i++){
-            if (clientes.get(i).getCpf() == cpf){
-                saida = "CPF pesquisado: "+clientes.get(i).getCpf()+"\n" +"\tNome: "+clientes.get(i).getNome()+"\n"+"\tEndereço: "+clientes.get(i).getEndereco()+"\n"+"\tEmail: "+clientes.get(i).getEmail()+"\n";
+        for (int i = 0; i < clientes.size(); i++) {
+            if (clientes.get(i).getCpf() == cpf) {
+                saida = "CPF pesquisado: " + clientes.get(i).getCpf() + "\n" + "\tNome: " + clientes.get(i).getNome() + "\n" + "\tEndereço: " + clientes.get(i).getEndereco() + "\n" + "\tEmail: " + clientes.get(i).getEmail() + "\n";
                 break;
             }
         }
         JOptionPane.showMessageDialog(null, saida);
     }
-    
-    //Recebe cpf do cliente e valida cpf, retorna true se cliente existe, false se nao existe
-    public boolean validaCPF(String cpf){
 
-        for (int i=0;i<clientes.size();i++)
-        {
-            if (clientes.get(i).getCpf().equals(cpf))
+    //Recebe cpf do cliente e valida cpf, retorna true se cliente existe, false se nao existe
+    public boolean validaCPF(String cpf) {
+
+        for (int i = 0; i < clientes.size(); i++) {
+            if (clientes.get(i).getCpf().equals(cpf)) {
                 return true;
+            }
         }
         return false;
     }
 
-    public void consultaCompras (String cpf, String dataInicial, String dataFinal){
+    public void consultaCompras(String cpf, String dataInicial, String dataFinal) {
         String saida = "CPF não encontrado.\n";
         int totalNotas;
-        for (int i=0;i<clientes.size();i++){
-            if (clientes.get(i).getCpf() == cpf){
-                saida = "CPF pesquisado: "+clientes.get(i).getCpf()+"\n";
-                saida += "Total de Notas Emitidas: "+clientes.get(i).getNotas().size()+"\n";
-                for (int j=0;j<clientes.get(i).getNotas().size();j++){
-                    saida += "Nota:\n"+"\tCódigo: "+clientes.get(i).getNotas().get(j).getCodigo()+"\tValor Total: "+clientes.get(i).getNotas().get(j).getValorTotalCompra()+"\n\n";
+        for (int i = 0; i < clientes.size(); i++) {
+            if (clientes.get(i).getCpf() == cpf) {
+                saida = "CPF pesquisado: " + clientes.get(i).getCpf() + "\n";
+                saida += "Total de Notas Emitidas: " + clientes.get(i).getNotas().size() + "\n";
+                for (int j = 0; j < clientes.get(i).getNotas().size(); j++) {
+                    saida += "Nota:\n" + "\tCódigo: " + clientes.get(i).getNotas().get(j).getCodigo() + "\tValor Total: " + clientes.get(i).getNotas().get(j).getValorTotalCompra() + "\n\n";
                 }
                 JOptionPane.showMessageDialog(null, saida);
                 break;
@@ -80,22 +80,41 @@ public class ControleCliente {
         }
     }
 
-    public void emitirNota(String cpf, Nota notaEmitida){
+    public void emitirNota(String cpf, Nota notaEmitida) {
         String produtosNota = "";
-        for (int i=0;i<clientes.size();i++)
-        {
+        for (int i = 0; i < clientes.size(); i++) {
             //procura usuario pelo cpf e coloca nota emitida no array de notas dele
-            if (clientes.get(i).getCpf().equals(cpf))
-            {
+            if (clientes.get(i).getCpf().equals(cpf)) {
                 clientes.get(i).getNotas().add(notaEmitida);
             }
         }
         //MOSTRA INFORMACOES DA NOTA
-        for(int i = 0;i < notaEmitida.getListaProduto().size(); i++){
+        for (int i = 0; i < notaEmitida.getListaProduto().size(); i++) {
             Produto produto = notaEmitida.getListaProduto().get(i);
             produtosNota += "Codigo: " + produto.getCodigo() + "\n";
         }
         JOptionPane.showMessageDialog(null, produtosNota);
+    }
+
+    public void consultaFatCliente(String cpf) {
+        String saida = "Cliente não encontrado.\n";
+        double fatTotal = 0;
+        boolean temCliente = false;
+        for (int i = 0; i < clientes.size(); i++) {
+            if (clientes.get(i).getCpf() == cpf) {
+                temCliente = true;
+                for (int j = 0; j < clientes.get(i).getNotas().size(); j++) {
+                    fatTotal += clientes.get(i).getNotas().get(j).getValorTotalCompra();
+                }
+            }
+        }
+        if (temCliente == false){
+            JOptionPane.showMessageDialog(null, saida);
+        }
+        else {
+            saida = "CPF pesquisado: "+cpf+"\n"+"Faturamento do cliente: "+fatTotal+"\n";
+            JOptionPane.showMessageDialog(null, saida);
+        }
     }
 
     public void gravarClientes() throws Exception {
@@ -111,7 +130,7 @@ public class ControleCliente {
         }
     }
 
-    public void lerClientes() throws Exception{
+    public void lerClientes() throws Exception {
         try {
             FileInputStream arquivo = new FileInputStream("clientes.ser");
             ObjectInputStream in = new ObjectInputStream(arquivo);
--- trabalho final/src/ControleNota.java
+++ trabalho final/src/ControleNota.java
@@ -1,22 +1,45 @@
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Gabriel
  */
 public class ControleNota {
-   ArrayList <Nota> notas = new ArrayList <>();
-   
-   public String emitirNota(){
-       
-       return null;
-       
-   }
+
+    ArrayList<Nota> notas = new ArrayList<>();
+
+    public void consultaFatProduto(int codProduto) {
+        String saida = "Produto não encontrado.\n";
+        boolean temProduto = false;
+        double fatTotal = 0;
+        for (int i = 0; i < notas.size(); i++) {
+            for (int j = 0; j < notas.get(i).getListaProduto().size(); j++) {
+                if (notas.get(i).getListaProduto().get(j).getCodigo() == codProduto) {
+                    temProduto = true;
+                    fatTotal += notas.get(i).getListaProduto().get(j).getValorDeVenda();
+                }
+
+            }
+        }
+        if (temProduto == false){
+            JOptionPane.showMessageDialog(null, saida);
+        }
+        else {
+            saida = "Código do produto pesquisado: "+codProduto+"\n"+"Faturamento do produto: R$ "+fatTotal+"\n";
+            JOptionPane.showMessageDialog(null, saida);
+        }
+    }
+
+    public String emitirNota() {
+
+        return null;
+
+    }
 }
