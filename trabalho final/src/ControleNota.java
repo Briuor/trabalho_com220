ef42447caddde58300ab155696dd5d435292d2c9
Gabriel Mouallem <gabriel.unifei2017@gmail.com>
2018-07-01 16:25:54 -0300

    Adicionadas anotações no controleNota para comunicação entre os Devs

--- trabalho final/src/ControleNota.java
+++ trabalho final/src/ControleNota.java
@@ -28,15 +28,35 @@ public class ControleNota {
 
             }
         }
-        if (temProduto == false){
+        if (temProduto == false) {
             JOptionPane.showMessageDialog(null, saida);
-        }
-        else {
-            saida = "Código do produto pesquisado: "+codProduto+"\n"+"Faturamento do produto: R$ "+fatTotal+"\n";
+        } else {
+            saida = "Código do produto pesquisado: " + codProduto + "\n" + "Faturamento do produto: R$ " + fatTotal + "\n";
             JOptionPane.showMessageDialog(null, saida);
         }
     }
 
+    /*
+    Coisa a se pensar: de que forma iremos consultar os 10 produtos mais vendidos?
+        Pela alteração quantidade no estoque talvez? Aparentemente viavel, mas tem muitos contras;
+        Fazendo FORs que percorrem todas as notas e todos os produtos somando os totais de cada um? Complicado e com problemas pra identificar
+            no final quais totais são de quais produtos
+        Alguma outra sugestão? Escrevam aqui que eu procuro fazer...
+    */
+    public void topDezMaisVendidos() {
+        for (int i = 0; i < notas.size(); i++) {
+            for (int j = 0; j < notas.get(i).getListaProduto().size(); j++) {
+
+            }
+        }
+    }
+
+    //Emitir nota é o responsável pelas compras dos clientes
+    /*
+    Coisas para lembrar:
+        Ao emitir uma nota deve-se alterar a quantEstoque do produto de acordo com a compra;
+        Uma nota pode ter no máximo 10 tipos de produtos diferentes;
+    */
     public String emitirNota() {
 
         return null;
